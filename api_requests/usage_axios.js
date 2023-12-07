05c3ab01c011912a80e119439f8792d55d038491
Dmytro <109982700+DmytroKosenko@users.noreply.github.com>
2023-12-07 20:52:17 +0200

    add_tests

--- api_requests/usage_axios.js
+++ api_requests/usage_axios.js
@@ -3,11 +3,13 @@ const { expect } = require("chai");
 const data = require("./data/dummy_data.json");
 const fs = require("fs-extra");
 
-describe("Actions for dummy website", async () => {
+describe("Actions for users on dummy website", async () => {
   let userId;
+  let userFirstName;
+  let userLastName;
 
-  //Create
-  it("Create user", async () => {
+  //Create user
+  it.skip("Create user", async () => {
     const createUser = await axios.post(
       `${data.baseUrl}/users/add`,
       {
@@ -26,7 +28,7 @@ describe("Actions for dummy website", async () => {
   });
 
   //Create product
-  it("Create product", async () => {
+  it.skip("Create product", async () => {
     const createProduct = await axios.post(
       `${data.baseUrl}/products/add`,
       {
@@ -43,20 +45,50 @@ describe("Actions for dummy website", async () => {
     expect(createProduct.status).equal(200);
   });
 
-  //Update
-  it.skip("Update user data", async () => {
-    const updateUserData = await axios.patch(
-      `${data.baseUrl}/users/101`,
-      {
-        username: "testUser",
-        password: "Qwe123",
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    console.log(updateUserData.data);
+  //Get users
+  it("Get users by search params", async () => {
+    const params = new URLSearchParams([
+      ["key", "hair.color"],
+      ["value", "Brown"],
+    ]);
+    const getUsersByParams = await axios.get(`${data.baseUrl}/users/filter`, {
+      params,
+    });
+    //console.log(getUsersByParams.data);
+    expect(getUsersByParams.status).equal(200);
+    userFirstName = getUsersByParams.data.users[2].firstName;
+    userLastName = getUsersByParams.data.users[2].lastName;
+    userId = getUsersByParams.data.users[2].id;
+    console.log(userFirstName);
+    //Arely
   });
+
+  //Get users by id and compare values
+  it("Get users by id and compare values", async () => {
+    const getUser = await axios.get(`${data.baseUrl}/users/${userId}`);
+    //console.log(getUser.data);
+    expect(getUser.status).equal(200);
+    expect(userFirstName).equal(getUser.data.firstName);
+    expect(userLastName).equal(getUser.data.lastName);
+  });
+
+  //Update user with id
+  // it.skip("Update user data", async (userId) => {
+  //   const updateUserData = await axios.patch(
+  //     `${data.baseUrl}/users/${userId}`,
+  //     {
+  //       username: "Marko",
+  //       password: "Polo",
+  //     },
+  //     {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${data.token}`,
+  //       },
+  //     }
+  //   );
+  //   console.log(updateUserData.data);
+  //   console.log(updateUserData.statusText);
+  //   console.log(updateUserData.status);
+  // });
 });
